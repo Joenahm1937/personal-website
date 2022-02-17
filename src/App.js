6eec3a429400446a5948fcb4479489b529a5172f
Joseph Nahm <josephnahm1@gmail.com>
2022-02-16 20:00:25 -0800

    snowfall and typing implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Snowfall from "react-snowfall";
+import AnimatedTyping from './components/AnimatedTyping'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Snowfall
+        color="#add8e6"
+        style={{}}
+        snowflakeCount={50}
+      />
+      <AnimatedTyping/>
     </div>
   );
 }
